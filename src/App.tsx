00e3f83a1b49eaec24c48d2e08d30123320808f2
Yaroslav <6715414@gmail.com>
2023-04-06 12:48:38 +0300

    added star background and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
+
+import { IMarker } from "./@types";
+import { ControlPanel } from "./components";
 
 function App() {
-  return <div className="App">App</div>;
+  const [activeMarkerId, setActiveMarkerId] = useState<string>("me");
+
+  const markers: IMarker[] = [
+    {
+      id: "me",
+      lat: 0,
+      log: 0,
+    },
+  ];
+  const activeMarker: IMarker | undefined = markers.find(
+    (marker) => marker.id === activeMarkerId
+  );
+
+  return (
+    <>
+      <div className="title">
+        <h1>3D Nearby Plane Traker</h1>
+      </div>
+      <Canvas
+        style={{
+          height: "calc(100vh - 170px)",
+          width: "100vw",
+        }}
+      >
+        <Stars />
+      </Canvas>
+      <div className="controls">
+        <ControlPanel
+          markers={markers}
+          activeMarkerId={activeMarkerId}
+          setActiveMarkerId={setActiveMarkerId}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
