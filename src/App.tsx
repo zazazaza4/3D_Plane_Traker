60cd004aeef7ea96bef6880f66accb6ffa0f0756
Yaroslav <6715414@gmail.com>
2023-04-30 14:30:22 +0300

    added userLocation in markers array

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,36 @@ import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Stars } from "@react-three/drei";
 
-import { IMarker } from "./@types";
+import { ICords, IMarker } from "./@types";
 import { ControlPanel, Earth } from "./components";
+import { useGeolocation } from "./hooks";
 
 function App() {
-  const [activeMarkerId, setActiveMarkerId] = useState<string>("me");
+  const userLocation: null | ICords = useGeolocation();
+  const [activeMarkerId, setActiveMarkerId] = useState<string>("aa01be");
 
-  const markers: IMarker[] = [
-    {
-      id: "me",
-      lat: 0,
-      log: 0,
-      type: "human",
-    },
-    {
-      id: "aa01be",
-      lat: 50.00156,
-      log: 36.231537,
-      type: "plane",
-    },
-  ];
+  const markers: IMarker[] = userLocation
+    ? [
+        {
+          id: "me",
+          type: "human",
+          ...userLocation,
+        },
+        {
+          id: "aa01be",
+          lat: 50.00156,
+          lon: 36.231537,
+          type: "plane",
+        },
+      ]
+    : [
+        {
+          id: "aa01be",
+          lat: 50.00156,
+          lon: 36.231537,
+          type: "plane",
+        },
+      ];
   const activeMarker: IMarker | undefined = markers.find(
     (marker) => marker.id === activeMarkerId
   );
