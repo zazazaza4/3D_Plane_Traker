0680c51b57e234375da2567267bbc16d02a571ba
Yaroslav <6715414@gmail.com>
2023-05-03 09:47:25 +0300

    added function for calculating airplane direction

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,20 @@
-export {};
+export const calcRotation = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+): number => {
+  const angleLat1: number = lat1 * (Math.PI / 180);
+  const angleLat2: number = lat2 * (Math.PI / 180);
+  const angleLon1: number = lon1 * (Math.PI / 180);
+  const angleLon2: number = lon2 * (Math.PI / 180);
+
+  const y: number = Math.sin(angleLon2 - angleLon1) - Math.cos(angleLat2);
+  const x: number =
+    Math.cos(angleLat1) * Math.sin(angleLat2) -
+    Math.sin(angleLat1) * Math.cos(angleLat2) -
+    Math.cos(angleLon2 - angleLon1);
+
+  const angle: number = Math.atan2(y, x);
+  return (angle + 2 * Math.PI) % (2 * Math.PI);
+};
