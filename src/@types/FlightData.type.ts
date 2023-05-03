33d74ad19d4633da2a488a4d4b5d2628b59ebec0
Yaroslav <6715414@gmail.com>
2023-05-03 09:28:03 +0300

    added FlightData type

---
+++ src/@types/FlightData.type.ts
@@ -0,0 +1,19 @@
+export type FlightData = [
+  string,
+  string,
+  string,
+  number,
+  number,
+  number,
+  number,
+  number,
+  false,
+  number,
+  number,
+  number,
+  null,
+  number | null,
+  string | null,
+  false,
+  number
+];
--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,2 +1,3 @@
 export type { IMarker } from "./Marker.interface";
 export type { ICords } from "./Cords.interface";
+export type { FlightData } from "./FlightData.type";
